bf65d7e46b25a8d5fec651382e2628a2739475d7
cdzsbin <zhangshibin@jd.com>
2022-02-20 22:55:17 +0800

    submit

--- vue.config.js
+++ vue.config.js
@@ -3,4 +3,5 @@ module.exports = defineConfig({
   transpileDependencies: true,
   outputDir: './docs/',
   indexPath: 'index.html',
+  publicPath: process.env.NODE_ENV === 'production' ? '/silent-chat/'  : '/'
 })
