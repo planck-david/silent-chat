e981ace00fc7e9104c155253745e1cfa01567736
planck-david <99948300+planck-david@users.noreply.github.com>
2022-02-21 10:42:45 +0800

    submit

--- vue.config.js
+++ vue.config.js
@@ -3,16 +3,5 @@ module.exports = defineConfig({
   transpileDependencies: true,
   outputDir: './docs/',
   indexPath: 'index.html',
-  publicPath: process.env.NODE_ENV === 'production' ? '/silent-chat/' : '/',
-  pages: {
-    index: {
-      entry: 'src/main.js',
-      filename: 'index.html',
-      cdn: {
-        css: [
-          'https://cdn.bootcss.com/material-design-icons/3.0.1/iconfont/material-icons.css'
-        ]
-      }
-    }
-  }
+  publicPath: process.env.NODE_ENV === 'production' ? '/silent-chat/' : '/'
 })
